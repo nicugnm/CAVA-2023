7aec9fbd71630524855f19307d2f4fd53c76bc4f
nicugnm <nghergu17@gmail.com>
2024-02-18 15:48:21 +0200

    add tema2

---
+++ TEMA2/CAVA-2023-TEMA2/solutie_personala/trimite_solutie/cod_solutie/Parameters.py
@@ -0,0 +1,27 @@
+import os
+
+class Parameters:
+    def __init__(self):
+        self.base_dir = './data'
+        self.dir_pos_examples = os.path.join(self.base_dir, 'exemple_pozitive_totale')
+        self.dir_neg_examples = os.path.join(self.base_dir, 'exemple_negative_totale')
+        self.dir_test_examples = os.path.join(self.base_dir,'exemple_test/validare/validare')
+        self.path_annotations = os.path.join(self.base_dir, 'exemple_test/validare/task2_wilma_gt_validare.txt')
+        self.dir_save_files = os.path.join(self.base_dir, 'salveazaFisiere')
+        if not os.path.exists(self.dir_save_files):
+            os.makedirs(self.dir_save_files)
+            print('directory created: {} '.format(self.dir_save_files))
+        else:
+            print('directory {} exists '.format(self.dir_save_files))
+
+        # set the parameters
+        self.dim_window = 72  # exemplele pozitive (fete de oameni cropate) au 36x36 pixeli
+        self.dim_hog_cell = 3  # dimensiunea celulei
+        self.dim_descriptor_cell = 72  # dimensiunea descriptorului unei celule
+        self.overlap = 0.3
+        self.use_flip_images = True
+        self.number_positive_examples = 997  # numarul exemplelor pozitive
+        self.number_negative_examples = 7890  # numarul exemplelor negative
+        self.overlap = 0.3
+        self.has_annotations = False
+        self.threshold = 0.1
